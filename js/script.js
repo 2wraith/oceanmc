718316e6a154bc7780c87ed45e0483daa4312c6e
seqentixx <137944241+seqentixx@users.noreply.github.com>
2023-06-29 16:08:39 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const config = {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "OceanMC", /*Server name*/
         serverIp: "play.oceanmc.fun", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
-        discordServerID: "1093860968724975660" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
+        discordServerID: "https://discord.gg/3bhHwd9b" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
     /*Admin-Team
